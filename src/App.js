ae3d0d82dce6d535ab2a9c21977c63ab5187da54
Gurdip Singh <singh.gurdip24@gmail.com>
2020-03-31 23:02:49 -0400

    Search button added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import './App.css';
 import { Component } from 'react';
 
+import Form from './components/Form';
+
 class App extends Component {
+  
+  getRecipe(e) {
+    e.preventDefault();
+    console.log(e.target.elements.recipeName.value);
+  }
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe}/>
       </div>
     );
   }
