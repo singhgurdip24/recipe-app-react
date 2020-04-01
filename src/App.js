18c7a1fd865b37c3369a54f5c793c81ccf82fb40
Gurdip Singh <singh.gurdip24@gmail.com>
2020-04-01 00:06:46 -0400

    api call + state added to app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Form from './components/Form';
 const API_KEY = "6eda369cac804921bb6b3c0ecc8a24a0";
 
 class App extends Component {
-  
+  state = {
+    recipes : []
+  }
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
@@ -16,7 +18,7 @@ class App extends Component {
                       https://api.spoonacular.com/recipes/search?query=${recipeName}
                       &apiKey=${API_KEY}`);
     const data = await api_call.json();
-    console.log(data);
+    this.setState({recipes : data.results});
   }
 
   render(){
@@ -26,6 +28,11 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe}/>
+        {
+          this.state.recipes.map (
+            recipe => <p key={ recipe.id }> { recipe.title } </p>
+          )
+        }
       </div>
     );
   }
