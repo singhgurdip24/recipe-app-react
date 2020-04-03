d6b6eae23c02b6ebc7a4b80f32684c97230ab9c0
Gurdip Singh <singh.gurdip24@gmail.com>
2020-04-02 22:19:04 -0400

    added boostrap and column for images

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
 const Recipe = props => (
-    <div>
+    <div className="container">
+        <div className="row">
         {
             props.recipes.map (recipe => {
                 return(
-                    <div key={ recipe.id }>
-                        <img src={`${props.imgUrl}${recipe.imageUrls}`} />
-                        <p> { recipe.title } </p>
-                    </div>
+                    <div className="col-md-4">
+                        <div className="recipe__box">
+                            <img
+                            className="recipe__box-img"
+                            src={`${props.imgUrl}${recipe.imageUrls}`}
+                            alt={recipe.title }
+                            />
+                            <div className="recipe__text">
+                                <h5>{recipe.title}</h5>
+                            </div>
+                        </div>
+                    </div>   
                 );
             })
         }
+        </div>
     </div>
 )
 
