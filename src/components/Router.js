bf6354acd834ab063adef1f51e5388a89b79c45a
Gurdip Singh <singh.gurdip24@gmail.com>
2020-04-04 18:40:51 -0400

    added eact router

---
+++ src/components/Router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Recipe from './Recipe';
+import App from '../App'
+import ViewRecipe from "./ViewRecipe";
+
+const Router = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={ App } exact />
+            <Route path="/recipe" component={ViewRecipe} />
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Router;
---
+++ src/components/ViewRecipe.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ViewRecipe = () => (
+    <div>
+        <h1>View Recipe</h1>
+    </div>
+);
+
+export default ViewRecipe;
